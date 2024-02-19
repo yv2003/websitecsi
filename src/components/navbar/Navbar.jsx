1ea90f0e883d2b11c98b23509b475c8fb00fec1a
Prachi Vaishya <vaishyaprachi14@gmail.com>
2024-02-20 00:43:11 +0530

    magazine section added

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -6,6 +6,25 @@ import { Link, NavLink } from "react-router-dom";
 export const Navbar = () => {
   const [menuOpen, setMenuOpen] = useState(false);
 
+  const redirectToBulletin = (event) => {
+    event.preventDefault();
+    window.location.href = 'https://online.pubhtml5.com/qesub/gwua/';    
+    // window.open('https://online.pubhtml5.com/qesub/gwua/', '_blank');    // for opening in new tab
+  };
+
+  const redirectToNimbus = (event) => {
+    event.preventDefault();
+    window.location.href = 'https://online.pubhtml5.com/qesub/ufmd/';    
+    // window.open('https://online.pubhtml5.com/qesub/ufmd/', '_blank');    // for opening in new tab
+  };
+
+  const redirectToNewsLetter = (event) => {
+    event.preventDefault();
+    window.location.href = 'https://online.pubhtml5.com/qesub/ufwi/';    
+    // window.open('https://online.pubhtml5.com/qesub/ufwi/', '_blank');    // for opening in new tab
+  };
+  
+
   return (
     <nav >
       <Link to="/" className="logo">
@@ -33,7 +52,7 @@ export const Navbar = () => {
         </li>
         <li>
           <NavLink to="">
-            <div className="dropdown rounded-md ">
+            <div className="dropdown rounded-md">
               EVENTS
               <div className="dropdown-content rounded-md">
                 <NavLink to="/events" className="a">2023-24</NavLink>
@@ -42,7 +61,16 @@ export const Navbar = () => {
           </NavLink>
         </li>
         <li>
-          <NavLink to="/magazine">MAGAZINE</NavLink>
+          <NavLink to="">
+            <div className="dropdown rounded-md">
+              MAGAZINE
+              <div className="dropdown-content rounded-md">
+                <span className="a" onClick={redirectToBulletin}>Bulletin</span>                
+                <span className="a" onClick={redirectToNimbus}>Nimbus</span>
+                <span className="a" onClick={redirectToNewsLetter}>NewsLetter</span>
+              </div>        
+            </div>
+          </NavLink>
         </li>
         <li>
           <NavLink to="/sponsors">SPONSORS</NavLink>
